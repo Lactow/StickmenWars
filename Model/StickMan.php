0b7894ee9d0195ba9ee34ac43a4313e031d11b9b
Lactor <carolyne.urbaniak@gmail.com>
2016-07-13 11:02:26 +0200

    Fix API
    Creating models

---
+++ Model/StickMan.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This class implements all methods for discussing with the API
+ * and instantiate StickMen
+ *
+ * @author lactor
+ */
+class StickMan {
+    public $idStickman;
+    public $nameStick;
+    protected $inventoryItems;
+    protected $Attributes;
+    protected $actionPoints;
+    protected $movePoints;
+    
+    
+    
+}
