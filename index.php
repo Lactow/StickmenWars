3f55889b435ee4060a4f7cb1bfc01e4c878078a4
Marc Masure <masure.marc.59@gmail.com>
2016-07-13 11:21:48 +0200

    add Index.php & web controllers

--- index.php
+++ index.php
@@ -1,17 +1,62 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        
-        ?>
-    </body>
-</html>
+<?php
+
+session_start();
+ob_start();
+
+//var_dump($_GET['link'],$_GET['a']);
+
+require('./Controllers/ViewController.php');
+
+$link  = new Index();
+$action = 1;
+
+if(isset($_GET['link']))
+{
+	switch($_GET['link'])
+	{
+		case 1 :
+		break;
+		case 'news-admin' :
+			require('./Controllers/NewsAdminController.php');
+			$link = new ();
+			break;
+		case 'home' :
+			require('./Controllers/Acceuilcontroller.php');
+			$link = new Home();
+			break;
+		case 'signup' :
+			require('./Controllers/SignUpController.php');
+			$link = new Signup();
+			break;
+		case 'connection' :
+			require('./Controllers/ConnectionController.php');
+			$link = new Connection();
+			break;
+		case 'script' :
+			require('./Controllers/ScriptController.php');
+			$link = new Script();
+			break;
+		case 'shop' :
+			require('./Controllers/ShopController.php');
+			$link = new Shop();
+			break;
+		case 'fight' :
+			require('./Controllers/FightController.php');
+			$link = new Fight();
+			break;
+		default :
+			break;
+	}
+}
+if(isset($_GET['a'])) $action = (int)$_GET['a'];
+$link->doAction($action);
+$contentPage = ob_get_contents();
+ob_end_clean();
+
+
+include('./view/HeaderView.html');
+include('./view/MenuView.html');
+//echo $contentPage;
+echo '<div id="content">'.$contentPage.'</div>';
+include('./view/FooterView.html');
+?>
