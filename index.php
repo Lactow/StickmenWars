09661400a8d5d90eb476d324f554c2e1eaff952f
mmasure <mmasure@mmasure-PC>
2016-08-23 09:48:51 +0200

    Index.php default redirection

--- index.php
+++ index.php
@@ -24,39 +24,47 @@ if(isset($_GET['link']))
 {
     switch($_GET['link'])
     {
-            case 'news-admin' :
-                    require('./Controllers/NewsAdminController.php');
-                    $link = new NewsAdmin();
-                    break;
-            case 'home' :
-                    require('./Controllers/HomeController.php');
-                    $link = new Home();
-                    break;
-            case 'signup' :
-                    require('./Controllers/SignUpController.php');
-                    $link = new Signup();
-                    break;
-            case 'connection' :
-                    require('./Controllers/ConnectionController.php');
-                    $link = new Connection();
-                    break;
-            case 'script' :
-                    require('./Controllers/ScriptController.php');
-                    $link = new Script();
-                    break;
-            case 'shop' :
-                    require('./Controllers/ShopController.php');
-                    $link = new Shop();
-                    break;
-            case 'fight' :
-                    require('./Controllers/FightController.php');
-                    $link = new Fight();
-                    break;
-            default :
-                    require('./Controllers/HomeController.php');
-                    $link = new Home();
-                    break;
+        case '':
+            require('./Controllers/HomeController.php');
+            $link = new Home();
+            break;
+        case 'home' :
+            require('./Controllers/HomeController.php');
+            $link = new Home();
+            break;
+        case 'news-admin' :
+            require('./Controllers/NewsAdminController.php');
+            $link = new NewsAdmin();
+            break;
+        case 'signup' :
+            require('./Controllers/SignUpController.php');
+            $link = new Signup();
+            break;
+        case 'connection' :
+            require('./Controllers/ConnectionController.php');
+            $link = new Connection();
+            break;
+        case 'script' :
+            require('./Controllers/ScriptController.php');
+            $link = new Script();
+            break;
+        case 'shop' :
+            require('./Controllers/ShopController.php');
+            $link = new Shop();
+            break;
+        case 'fight' :
+            require('./Controllers/FightController.php');
+            $link = new Fight();
+            break;
+        default :
+            require('./Controllers/HomeController.php');
+            $link = new Home();
+            break;
     }
+    //@TODO : default "/" redirection must be review 
+} else {
+    require('./Controllers/HomeController.php');
+    $link = new Home();
 }
 
 if(isset($_GET['a'])) $action = (int)$_GET['a'];
